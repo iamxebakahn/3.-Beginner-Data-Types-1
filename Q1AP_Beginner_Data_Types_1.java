909d7b6355ae537d096a2d568387c79b18f02b67
iamxebakahn <zebadwight@gmail.com>
2025-05-14 19:42:29 +0530

    Update Q1AP_Beginner_Data_Types_1.java

--- Q1AP_Beginner_Data_Types_1.java
+++ Q1AP_Beginner_Data_Types_1.java
@@ -1,3 +1,40 @@
+// Problem Description
+
+// Print the following pattern in output
+//  *
+// * *
+//  *
+
+
+// Problem Constraints
+
+// No constraints
+
+
+// Input Format
+
+// No need to take input.
+
+
+// Output Format
+
+// Print the following pattern in 3 lines, in which first and last line has 1 spaces and then 1 star, and middle line has 1 star 1 space and 1 star.
+//  *
+// * *
+//  *
+
+
+// Example Input
+
+// No input required.
+
+
+// Example Output
+
+// Output :
+//  *
+// * *
+//  *
 import java.lang.*;
 import java.util.*;
 
