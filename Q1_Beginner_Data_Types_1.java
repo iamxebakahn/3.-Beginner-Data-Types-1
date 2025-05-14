dab426706dd2a356c191dc63059ef8318883a7af
iamxebakahn <zebadwight@gmail.com>
2025-05-14 19:39:29 +0530

    Add files via upload

---
+++ Q1_Beginner_Data_Types_1.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import java.util.*;
+
+public class Q1_Beginner_Data_Types_1{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        for(int i=1;i<=4;i++)
+        {
+            switch(i)
+            {
+                case 1: System.out.println("**********");
+                        break;
+                case 2: System.out.println("*        *");
+                        break;
+                case 3: System.out.println("*        *");
+                        break;
+                case 4: System.out.println("**********");
+                        break;
+            }
+        }
+        
+    }
+}
