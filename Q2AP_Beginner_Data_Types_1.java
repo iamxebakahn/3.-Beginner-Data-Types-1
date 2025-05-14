a1335d621506de7cdc646429fe3a5e63ad040b13
iamxebakahn <zebadwight@gmail.com>
2025-05-14 19:46:03 +0530

    Add files via upload

---
+++ Q2AP_Beginner_Data_Types_1.java
@@ -0,0 +1,18 @@
+
+import java.lang.*;
+import java.util.*;
+
+public class Q2AP_Beginner_Data_Types_1{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+
+        for (int i=1;i<=5;i++)
+        {
+            System.out.println("3*"+i+" = "+(3*i));
+        }
+        
+    }
+}
